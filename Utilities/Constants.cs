02d7b54554fec1f4fdb9e43b60b9870a97c48b82
Danilo <danilozacyac@gmail.com>
2013-06-27 23:19:20 -0500

    Utilidades
    Creación de la biblioteca de clases Utilities

---
+++ Utilities/Constants.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace Utilities
+{
+    public class Constants
+    {
+        /// <summary>
+        /// Patrón regex de válidez de dirección de correo
+        /// </summary>
+        public const string MatchEmailPattern = @"^(([\w-]+\.)+[\w-]+|([a-zA-Z]{1}|[\w-]{2,}))@" +
+                                                @"((([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\.([0-1]?
+				[0-9]{1,2}|25[0-5]|2[0-4][0-9])\." +
+                                                @"([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\.([0-1]?
+				[0-9]{1,2}|25[0-5]|2[0-4][0-9])){1}|" +
+                                                @"([a-zA-Z]+[\w-]+\.)+[a-zA-Z]{2,4})$";
+
+        /// <summary>
+        /// Description
+        ///  Password entre 8 and 20 caracteres; debe contener una letra minúscula, una mayúscula, 
+        /// un número, y un caracter especial, no puede contener espacios en blanco
+        /// </summary>
+        public const String StrongPass = @"^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,20}$";
+
+    }
+}
---
+++ Utilities/MisFunct.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Utilities
+{
+    public class MisFunct
+    {
+        /// <summary>
+        /// Verifica que la cadena que se ingreso como e-mail tenga un patrón válido
+        /// </summary>
+        /// <param name="email"></param>
+        /// <returns></returns>
+        public static bool IsEmail(string email)
+        {
+            return (Regex.IsMatch(email, Constants.MatchEmailPattern) == true || email.Equals("")) ? true : false;
+        }
+
+        /// <summary>
+        /// Verifica que el caracter ingresado sea un caracter numérico
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        public static bool IsADigit(string text)
+        {
+            Regex regex = new Regex("[^0-9.-]+"); //regex that matches disallowed text 
+            return regex.IsMatch(text);
+        }
+    }
+}
