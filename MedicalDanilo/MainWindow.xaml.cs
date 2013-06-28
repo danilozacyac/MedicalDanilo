baeecedaf6c633e6457580b8300f13d6d44d0b84
Danilo <danilozacyac@gmail.com>
2013-06-27 23:24:35 -0500

    LogIn y Desktop

--- MedicalDanilo/MainWindow.xaml.cs
+++ MedicalDanilo/MainWindow.xaml.cs
@@ -1,16 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace MedicalDanilo
 {
@@ -22,6 +12,22 @@ namespace MedicalDanilo
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnAceptar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnCancelar_Click(object sender, RoutedEventArgs e)
+        {
+            //FrmCatalogoMedicamentos catalogo = new FrmCatalogoMedicamentos();
+            //catalogo.Show();
         }
     }
 }
