13275c51b812a246d9c21215aab12159aa2a112c
Danilo <danilozacyac@gmail.com>
2013-06-29 00:52:01 -0500

    Clase y formularios

---
+++ MedicalDanilo/Dao/OrdenLaboratorio.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Linq;
+
+namespace MedicalDanilo.Dao
+{
+    public class OrdenLaboratorio
+    {
+        private int idOrden;
+        private int idLaboratorio;
+        private int idPaciente;
+        private int idConsulta;
+        private DateTime fechaEnvio;
+        private DateTime fechaEntrega;
+        private bool entregaATiempo;
+        private double importeTotal;
+        private double aCuenta;
+        private String observaciones;
+
+        public OrdenLaboratorio(int idOrden, int idLaboratorio, int idPaciente, int idConsulta, DateTime fechaEnvio, DateTime fechaEntrega, bool entregaATiempo, double importeTotal, double aCuenta, String observaciones)
+        {
+            this.idOrden = idOrden;
+            this.idLaboratorio = idLaboratorio;
+            this.idPaciente = idPaciente;
+            this.idConsulta = idConsulta;
+            this.fechaEnvio = fechaEnvio;
+            this.fechaEntrega = fechaEntrega;
+            this.entregaATiempo = entregaATiempo;
+            this.importeTotal = importeTotal;
+            this.aCuenta = aCuenta;
+            this.observaciones = observaciones;
+        }
+
+        public OrdenLaboratorio()
+        {
+        }
+
+        public int IdOrden
+        {
+            get
+            {
+                return this.idOrden;
+            }
+            set
+            {
+                this.idOrden = value;
+            }
+        }
+
+        public int IdLaboratorio
+        {
+            get
+            {
+                return this.idLaboratorio;
+            }
+            set
+            {
+                this.idLaboratorio = value;
+            }
+        }
+
+        public int IdPaciente
+        {
+            get
+            {
+                return this.idPaciente;
+            }
+            set
+            {
+                this.idPaciente = value;
+            }
+        }
+
+        public int IdConsulta
+        {
+            get
+            {
+                return this.idConsulta;
+            }
+            set
+            {
+                this.idConsulta = value;
+            }
+        }
+
+        public DateTime FechaEnvio
+        {
+            get
+            {
+                return this.fechaEnvio;
+            }
+            set
+            {
+                this.fechaEnvio = value;
+            }
+        }
+
+        public DateTime FechaEntrega
+        {
+            get
+            {
+                return this.fechaEntrega;
+            }
+            set
+            {
+                this.fechaEntrega = value;
+            }
+        }
+
+        public bool EntregaATiempo
+        {
+            get
+            {
+                return this.entregaATiempo;
+            }
+            set
+            {
+                this.entregaATiempo = value;
+            }
+        }
+
+        public double ImporteTotal
+        {
+            get
+            {
+                return this.importeTotal;
+            }
+            set
+            {
+                this.importeTotal = value;
+            }
+        }
+
+        public double ACuenta
+        {
+            get
+            {
+                return this.aCuenta;
+            }
+            set
+            {
+                this.aCuenta = value;
+            }
+        }
+
+        public string Observaciones
+        {
+            get
+            {
+                return this.observaciones;
+            }
+            set
+            {
+                this.observaciones = value;
+            }
+        }
+    }
+}
---
+++ MedicalDanilo/Formularios/Laboratorios/FrmOrdenLaboratorios.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+using Telerik.Windows.Controls;
+
+namespace MedicalDanilo.Formularios.Laboratorios
+{
+    /// <summary>
+    /// Interaction logic for FrmOrdenLaboratorios.xaml
+    /// </summary>
+    public partial class FrmOrdenLaboratorios
+    {
+        public FrmOrdenLaboratorios()
+        {
+            InitializeComponent();
+        }
+
+        private void RadWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TxtImporte_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+
+        }
+
+        private void RadPagado_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
